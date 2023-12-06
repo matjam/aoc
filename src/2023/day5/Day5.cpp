86884640da3204742d3de0161d59e02dbb64d254
Nathan Ollerenshaw <nollerenshaw@nerdwallet.com>
2023-12-06 09:38:31 -0800

    oof

--- src/2023/day5/Day5.cpp
+++ src/2023/day5/Day5.cpp
@@ -15,8 +15,6 @@
 #define DEBUG 1
 #define TEST_FILE 0
 
-void solve(int part);
-
 using namespace std;
 
 Day5::Day5(AOCRenderer *r)
@@ -26,18 +24,16 @@ Day5::Day5(AOCRenderer *r)
 
 void Day5::run()
 {
-    // string day5input;
+    string day5input;
 
-    // if (TEST_FILE)
-    //     day5input = "resources/day5_sample.txt";
-    // else
-    //     day5input = "resources/day5_input.txt";
+    if (TEST_FILE)
+        day5input = "resources/day5_sample.txt";
+    else
+        day5input = "resources/day5_input.txt";
 
-    // load_data(day5input);
+    load_data(day5input);
 
     // part2();
-
-    solve(2);
 }
 
 void Day5::load_data(string filename)
@@ -204,80 +200,4 @@ void Day5::part1()
 
     cout << "day 5 part 1 lowest location: " << lowest << endl;
     cout << "completed in " << elapsed.asMilliseconds() << "ms" << endl;
-}
-
-// I fucking give up
-void solve(int part)
-{
-    std::set<std::pair<int64_t, int64_t>> cur, keep, next; // Sets of ranges in the form (start, len)
-    std::ifstream f("resources/day5_input.txt");
-    if (!f.is_open())
-    {
-        return;
-    }
-    // Parse the seeds, depending on the input
-    std::string line;
-    std::getline(f, line);
-    std::istringstream line_stream(line);
-    std::string seed_start, seed_len;
-    std::getline(line_stream, seed_start, ' '); // Throw away "seeds: " from first line
-    while (std::getline(line_stream, seed_start, ' '))
-    {
-        if (part == 1) // In part 1, treat the seeds like seed ranges of length 1
-        {
-            cur.insert({std::stoll(seed_start), 1});
-        }
-        else // In part 2, use the seed value pairs
-        {
-            std::getline(line_stream, seed_len, ' ');
-            cur.insert({std::stoll(seed_start), std::stoll(seed_len)});
-        }
-    }
-    // Parse and apply the piping lines
-    while (std::getline(f, line))
-    {
-        if (line.empty()) // If the line is empty, prepare to start the next map
-        {
-            cur.insert(next.begin(), next.end());
-            next.clear();
-            std::getline(f, line); // Throw away "X-to-Y map:\n"
-            continue;
-        }
-        int64_t pipe_dest, pipe_start, pipe_len;
-        line_stream.clear(); // Necessary to reuse line_stream
-        line_stream.str(line);
-        line_stream >> pipe_dest >> pipe_start >> pipe_len;
-        // Handle the current ranges, piping and splitting if necessary
-        for (auto [range_start, range_len] : cur)
-        {
-            // Find the lower and upper positions of the intersection between this
-            // range and the pipe
-            int64_t lower = std::max(range_start, pipe_start);
-            int64_t upper = std::min(range_start + range_len, pipe_start + pipe_len);
-            if (upper > lower)
-            {
-                // Push the part of the range in the pipe through the pipe
-                next.insert({pipe_dest - pipe_start + lower, upper - lower});
-                // Keep the upper/lower portions of the range if not in pipe
-                if (range_start + range_len > pipe_start + pipe_len)
-                {
-                    keep.insert({pipe_start + pipe_len, range_start + range_len - pipe_start - pipe_len});
-                }
-                if (range_start < pipe_start)
-                {
-                    keep.insert({range_start, pipe_start - range_start});
-                }
-            }
-            else // The range and pipe do not overlap, so keep the whole range
-            {
-                keep.insert({range_start, range_len});
-            }
-        }
-        cur.swap(keep);
-        keep.clear();
-    }
-    cur.insert(next.begin(), next.end()); // Include outputs from final iteration
-    f.close();
-    // Output the start of the "lowest" range (using the fact that cur is sorted)
-    std::cout << "Part " << part << ": " << cur.begin()->first << std::endl;
 }
